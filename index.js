7a21d639ccf7d3cd809e7202f5e0496bca4f8f93
Peter Orum <peter@peterorum.com>
2019-09-11 15:29:44 +1000

    add dictionary api

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
+const fetch = require('node-fetch')
 var Twit = require('twit')
 
 const { words } = require('./words.js')
 
-function post() {
+async function getMeaning(word) {
+    const response = await fetch(
+        `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${process.env.dictionary_api_key}`
+    )
+    const json = await response.json()
+
+    let meaning = ''
+
+    if (json.length && json[0].shortdef) {
+        const meanings = json.reduce((arr, m) => [...arr, ...m.shortdef], [])
+
+        meaning = meanings[Math.floor(Math.random() * meanings.length)]
+    }
+
+    return meaning
+}
+
+async function post() {
     // pick one at random
     const word = words[Math.floor(Math.random() * words.length)]
+
+    const meaning = await getMeaning(word)
+
+    const text = meaning ? `${word}: ${meaning}` : word
 
     const T = new Twit({
         consumer_key: process.env.tw_jzx_consumer_key,
@@ -15,19 +37,21 @@ function post() {
 
     // tweet
 
-    T.post('statuses/update', { status: word }, function(err) {
-
+    T.post('statuses/update', { status: text }, function(err) {
         if (err) {
             console.log(err)
         }
     })
 
-    return word;
+    return word
 }
 
-// lambda
-exports.handler = function( event, context, callback ) {
-    const word = post();
+// test - remove before deploying
+// post()
 
-    callback(null, `posted ${word}`);
+// lambda
+exports.handler = function(event, context, callback) {
+    const word = post()
+
+    callback(null, `posted ${word}`)
 }
